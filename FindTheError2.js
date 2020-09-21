d7b9e3d895f821da14b6e9c3cb5fc39940992456
KariukiBrian <brayoknight@gmail.com>
2020-09-20 17:47:42 -0700

    debug2

---
+++ FindTheError2.js
@@ -0,0 +1,20 @@
+// javascript code
+//
+// script_name: Find The Error 2
+//
+// author: The EarSketch Team
+//
+// description: Find and fix the error in this script
+//
+//ReferenceError - case sensitivity
+
+init();
+setTempo(88);
+
+var electroString = Y13_STRING_1;
+var drums = HIPHOP_DUSTYGROOVE_017;
+
+fitMedia(electroString, 1, 1, 9);
+fitMedia(drums, 2, 5, 9);
+
+finish();
