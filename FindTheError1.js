7763264fcebece6621df5ee2c816c10a2a29e69c
KariukiBrian <brayoknight@gmail.com>
2020-09-20 17:47:13 -0700

    debug1

---
+++ FindTheError1.js
@@ -0,0 +1,21 @@
+// javascript code
+//
+// script_name: Find The Error 1
+//
+// author: The EarSketch Team
+//
+// description: Find and fix the error in this script
+//
+
+//syntax error - placing the cart before the horse/calling functions before variables
+
+init();
+setTempo(88);
+
+var electroString = Y13_STRING_1;
+var drums = HIPHOP_DUSTYGROOVE_017;
+
+fitMedia(electroString, 1, 1, 9);
+fitMedia(drums, 2, 5, 9);
+
+finish();
